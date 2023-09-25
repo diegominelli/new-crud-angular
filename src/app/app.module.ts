f836b2473b36c1b282a81d24e4521e04aaaf4b10
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-09-25 20:23:50 -0300

    criacao e desenvolvimento do filtro da tabela de contatos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { StartsComponent } from './contacts/starts/starts.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, ContactListComponent, StartsComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
